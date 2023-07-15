0ae0c10a93b95750dca585e05036d6f7d5919254
Daniildo <daniil_titovsk@mail.ru>
2023-07-15 14:55:00 +0300

    First version

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import '../bootstrap/css/bootstrap.css';
+import '../styles/index.scss';
+import '../styles/adaptive.scss';
+
+console.log('webpack starterkit');
+
+import 'animate.css';
+const WOW = require('wow.js');
+
+new WOW().init();
